b102ccb31cfb2bf95257b33fc2279f37a600d7be
dotevo <dotevo@gmail.com>
2016-10-25 21:10:49 +0200

    Aldoni serĉilon
    Simpla UDP servilo kun elsendo

--- ĉefa.js
+++ ĉefa.js
@@ -2,9 +2,13 @@ const {app} = require('electron')
 const Fenestro = require('./fontkodo/fenestro')
 const Apleto = require('./fontkodo/apleto')
 
+const Serĉilo = require('./fontkodo/serĉilo')
+new Serĉilo()
+
 function krei() {
 	var fenestro = new Fenestro()
 	new Apleto({'fenestro':fenestro})
 }
+
 
 app.on('ready', krei)
