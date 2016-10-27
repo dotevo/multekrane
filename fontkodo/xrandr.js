a5b2749eeb3ba68cd32728e35cb7161d5228d7ad
Adam Jordanek <dotevo@gmail.com>
2016-10-27 12:34:46 +0200

    Aldoni XRandr

---
+++ fontkodo/xrandr.js
@@ -0,0 +1,39 @@
+const infano = require('child_process')
+const EventEmitter = require('events')
+
+let ex = {
+	kampoj: {1: 'nomo', 2: 'stato', 3: 'tipo', 4: 'larĝo', 5: 'alto', 6: 'x', 7: 'y'},
+	re: /^(\S+) ((?:dis)?connected) ([a-z]+)?\s?(?:(\d+)x(\d+))?(?:\+(\d+)\+(\d+))?/
+}
+
+class XRandR extends EventEmitter{
+	constructor() {
+		super()
+	}
+
+	akiriEkranojn(revoko) {
+		if(!revoko) return;
+
+		infano.exec('xrandr', function (err, stdout) {
+			let linio = stdout.split('\n')
+			let ekranoj = []
+			linio.forEach(function (line) {
+				let m = ex.re.exec(line)
+				if (m) {
+					let ekrano = {}
+					for(var i=0;i<m.length;++i){
+						if(ex.kampoj[i] != null) {
+							ekrano[ex.kampoj[i]] = m[i]
+						}
+					}
+					ekranoj.push(ekrano)
+				}
+
+			});
+			revoko(ekranoj)
+		});
+	}
+}
+
+
+module.exports = new XRandR()
--- ĉefa.js
+++ ĉefa.js
@@ -5,6 +5,9 @@ const Apleto = require('./fontkodo/apleto')
 const Serĉilo = require('./fontkodo/serĉilo')
 new Serĉilo()
 
+const XRandR = require('./fontkodo/xrandr')
+XRandR.akiriEkranojn(function(ekranoj) {console.log(ekranoj)} )
+
 function krei() {
 	var fenestro = new Fenestro()
 	new Apleto({'fenestro':fenestro})
